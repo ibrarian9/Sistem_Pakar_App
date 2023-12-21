fc2c1849b959ee50664a1b5869d53f4b07715704
ibrarian9 <ibrarian9@gmail.com>
2023-12-22 03:02:34 +0700

    Persentase Belum dibikin

--- app/src/main/java/com/app/sistempakar/HasilActivity.java
+++ app/src/main/java/com/app/sistempakar/HasilActivity.java
@@ -3,22 +3,42 @@ package com.app.sistempakar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 import java.util.ArrayList;
 
 public class HasilActivity extends AppCompatActivity {
 
     TextView nama;
+    ImageView gambar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_hasil);
 
-
-        String data = getIntent().getStringExtra("R1");
+        String data = getIntent().getStringExtra("data");
         nama = findViewById(R.id.tvNama);
         nama.setText(data);
+
+        gambar = findViewById(R.id.image);
+        assert data != null;
+        switch (data) {
+            case "Mawar":
+                Picasso.get().load(R.drawable.mawar).fit().into(gambar);
+                break;
+            case "Anggrek":
+                Picasso.get().load(R.drawable.anggrek).fit().into(gambar);
+                break;
+            case "Lily":
+                Picasso.get().load(R.drawable.lily).fit().into(gambar);
+                break;
+            case "Matahari":
+                Picasso.get().load(R.drawable.matahari).fit().into(gambar);
+                break;
+        }
     }
 }
--- app/src/main/java/com/app/sistempakar/MainActivity.java
+++ app/src/main/java/com/app/sistempakar/MainActivity.java
@@ -1,94 +1,202 @@
 package com.app.sistempakar;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.app.sistempakar.Adapter.SistemAdapter;
-import com.app.sistempakar.Models.DataPakar;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
-import com.google.gson.Gson;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
-    RecyclerView rv;
-    TextView submit;
-    SistemAdapter sistemAdapter;
-    ArrayList<DataPakar> list = new ArrayList<>();
-    ArrayList<String> newData = new ArrayList<>();
+    TextView question, tv, yes, no;
+    int num = 1;
 
+    @SuppressLint("SetTextI18n")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        sistemAdapter = new SistemAdapter(this, list);
-        rv = findViewById(R.id.rv);
-        rv.setAdapter(sistemAdapter);
-        rv.setLayoutManager(new LinearLayoutManager(this));
+        String R1 = "Apakah Bunga Berwarna Merah ?";
+        String R2 = "Apakah Bunga berwarna kuning ?";
+        String R3 = "Apakah Memiliki aroma yang harum ?";
+        String R4 = "Apakah Tidak memiliki aroma yang harum ?";
+        String R5A = "Apakah memiliki Batang kuat ?";
+        String R5B = "Apakah memiliki Batang kuat ?";
+        String R6 = "Apakah memiliki Batang lemah ?";
+        String R7 = "Apakah memiliki Batang berduri ?";
+        String R8A = "Apakah Batang tidak berduri ?";
+        String R8B = "Apakah Batang tidak berduri ?";
+        String R9 = "Apakah Tumbuh di dataran tinggi ?";
+        String R10 = "Apakah Tumbuh di dataran rendah ?";
+        String R11 = "Apakah Kelopak tersusum rapat ?";
+        String R12 = "Apakah Daun tumbuh bergerombol ?";
+        String R13A = "Apakah Daun tumbuh tidak bergerombol ?";
+        String R13B = "Apakah Daun tumbuh tidak bergerombol ?";
+        String R14A = "Apakah Waktu mekar yang tidak lama ?";
+        String R14B = "Apakah Waktu mekar yang tidak lama ?";
+        String R15 = "Apakah Waktu mekar yang lama ?";
+        String R16 = "Apakah memerlukan Pencahayaan parsial ?";
+        String R17 = "Apakah memerlukan Pencahayaan tidak parsial ?";
+        String R18A = "Apakah Ukuran bunga besar ?";
+        String R18B = "Apakah Ukuran bunga besar ?";
+        String R19 = "Apakah Ukuran bunga sedang ?";
+        String R20 = "Apakah Perawatannya mudah ?";
+        String R21 = "Apakah Harus diberikan perawatan ekstra ?";
+        String R22 = "Apakah Akar serabut ?";
+        String R23 = "Apakah Akar tunggang ?";
 
-        ValueEventListener value = new ValueEventListener() {
-            @SuppressLint("NotifyDataSetChanged")
-            @Override
-            public void onDataChange(@NonNull DataSnapshot snapshot) {
-                sistemAdapter.clear();
+        question = findViewById(R.id.question);
+        tv = findViewById(R.id.tv);
+        yes = findViewById(R.id.yes);
+        no = findViewById(R.id.no);
 
-                for (DataSnapshot snapshot1 : snapshot.getChildren()) {
-                    DataPakar dataPakarList = snapshot1.getValue(DataPakar.class);
-                    list.add(dataPakarList);
-                }
-                sistemAdapter.notifyDataSetChanged();
+        question.setText(R1);
+        yes.setOnClickListener( v -> {
+            num++;
+            tv.setText("Pertanyaan " + num);
+            if (question.getText().equals(R1)) {
+                question.setText(R3);
+            } else if (question.getText().equals(R3)) {
+                question.setText(R5A);
+            } else if (question.getText().equals(R5A)) {
+                question.setText(R10);
+            } else if (question.getText().equals(R10)) {
+                question.setText(R14A);
+            } else if (question.getText().equals(R14A)) {
+                question.setText(R21);
+            } else if (question.getText().equals(R21)) {
+                question.setText(R7);
+            } else if (question.getText().equals(R7)) {
+                question.setText(R11);
+            } else if (question.getText().equals(R11)) {
+                intentMawar();
+            } else if (question.getText().equals(R8A)) {
+                question.setText(R13A);
+            } else if (question.getText().equals(R13A)) {
+                question.setText(R18A);
+            } else if (question.getText().equals(R18A)) {
+               intentLily();
+            } else if (question.getText().equals(R2)) {
+                question.setText(R4);
+            } else if (question.getText().equals(R4)) {
+                question.setText(R8B);
+            } else if (question.getText().equals(R8B)) {
+                question.setText(R17);
+            } else if (question.getText().equals(R17)) {
+                question.setText(R6);
+            } else if (question.getText().equals(R6)) {
+                question.setText(R12);
+            } else if (question.getText().equals(R12)) {
+                question.setText(R15);
+            } else if (question.getText().equals(R15)) {
+                question.setText(R19);
+            } else if (question.getText().equals(R19)) {
+                question.setText(R23);
+            } else if (question.getText().equals(R23)) {
+               intentAnggrek();
+            } else if (question.getText().equals(R9)) {
+                question.setText(R5B);
+            } else if (question.getText().equals(R5B)) {
+                question.setText(R13B);
+            } else if (question.getText().equals(R13B)) {
+                question.setText(R14B);
+            } else if (question.getText().equals(R14B)) {
+                question.setText(R18B);
+            } else if (question.getText().equals(R18B)) {
+                question.setText(R20);
+            } else if (question.getText().equals(R20)) {
+                question.setText(R22);
+            } else if (question.getText().equals(R22)) {
+                intentMatahari();
             }
-
-            @Override
-            public void onCancelled(@NonNull DatabaseError error) {
-
-            }
-        };
-
-        Query listData = FirebaseDatabase.getInstance().getReference("Data");
-        listData.addValueEventListener(value);
-
-        String[] rules1 = new String[]{"A1","A3","A6","A7","A9","A16","A18"};
-        String[] rules2 = new String[]{"A2","A5","A10","A15","A20"};
-        String[] rules3 = new String[]{"A3","A7","A11","A13","A17"};
-        String[] rules4 = new String[]{"A4","A8","A12","A14","A19"};
-
-        System.out.println(Arrays.toString(rules1));
-
-        submit = findViewById(R.id.tvSubmit);
-        submit.setOnClickListener(v -> {
-            ArrayList<DataPakar> dataPakars = sistemAdapter.getAll();
-            for (int i = 0; i < dataPakars.size(); i++) {
-                DataPakar dataPakar = dataPakars.get(i);
-                if (dataPakar.isSelected()) {
-                    Intent in = new Intent(this, HasilActivity.class);
-                    ArrayList<String> newData = new ArrayList<>();
-                    newData.add(dataPakar.getId());
-                    System.out.println(newData);
-                    if (newData.equals(rules2)) {
-                        System.out.println("data Sama");
-                    } else {
-                        System.out.println("data tidak Sama");
-                    }
-
-                }}
         });
-        }
+
+        no.setOnClickListener( v -> {
+            num++;
+            tv.setText("Pertanyaan " + num);
+            if (question.getText().equals(R3)){
+                question.setText(R5A);
+            } else if (question.getText().equals(R5A)) {
+                question.setText(R8A);
+            } else if (question.getText().equals(R8A)) {
+                question.setText(R13A);
+            } else if (question.getText().equals(R13A)) {
+                question.setText(R18A);
+            } else if (question.getText().equals(R18A)) {
+                intentLily();
+            } else if (question.getText().equals(R10)) {
+                question.setText(R14A);
+            } else if (question.getText().equals(R14A)) {
+                question.setText(R16);
+            } else if (question.getText().equals(R16)) {
+                question.setText(R21);
+            } else if (question.getText().equals(R21)) {
+                question.setText(R7);
+            } else if (question.getText().equals(R7)) {
+                question.setText(R11);
+            } else if (question.getText().equals(R11)) {
+                intentMawar();
+            } else if (question.getText().equals(R17)) {
+                question.setText(R6);
+            } else if (question.getText().equals(R6)) {
+                question.setText(R12);
+            } else if (question.getText().equals(R12)) {
+                question.setText(R15);
+            } else if (question.getText().equals(R15)) {
+                question.setText(R19);
+            } else if (question.getText().equals(R19)) {
+                question.setText(R23);
+            } else if (question.getText().equals(R23)) {
+                intentAnggrek();
+            } else if (question.getText().equals(R1)) {
+                question.setText(R2);
+            } else if (question.getText().equals(R2)) {
+                question.setText(R4);
+            } else if (question.getText().equals(R4)) {
+                question.setText(R8B);
+            } else if (question.getText().equals(R8B)) {
+                question.setText(R9);
+            } else if (question.getText().equals(R9)) {
+                question.setText(R5B);
+            } else if (question.getText().equals(R5B)) {
+                question.setText(R13B);
+            } else if (question.getText().equals(R13B)) {
+                question.setText(R14B);
+            } else if (question.getText().equals(R14B)) {
+                question.setText(R18B);
+            } else if (question.getText().equals(R18B)) {
+                question.setText(R20);
+            } else if (question.getText().equals(R20)) {
+                question.setText(R22);
+            } else if (question.getText().equals(R22)) {
+                intentMatahari();
+            }
+        });
+    }
+
+    private void intentMawar() {
+        Intent i = new Intent(this, HasilActivity.class);
+        i.putExtra("data", "Mawar");
+        startActivity(i);
+        finish();
+    }
+    private void intentAnggrek() {
+        Intent i = new Intent(this, HasilActivity.class);
+        i.putExtra("data", "Anggrek");
+        startActivity(i);
+        finish();
+    } private void intentLily() {
+        Intent i = new Intent(this, HasilActivity.class);
+        i.putExtra("data", "Lily");
+        startActivity(i);
+        finish();
+    } private void intentMatahari() {
+        Intent i = new Intent(this, HasilActivity.class);
+        i.putExtra("data", "Matahari");
+        startActivity(i);
+        finish();
+    }
 }
